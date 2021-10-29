a89b47a2ce6d428919541f768400c5364a6512e7
FantasticBoyZ <khoidragonslayer@gmail.com>
2021-10-29 23:52:57 +0700

    update checkout screen

---
+++ app/src/main/java/fptu/ninhtbm/thebookshop/ui/home/CartActivity.java
@@ -0,0 +1,16 @@
+package fptu.ninhtbm.thebookshop.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fptu.ninhtbm.thebookshop.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
