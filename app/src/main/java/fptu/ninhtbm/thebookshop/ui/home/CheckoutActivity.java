03e20817095bb1bb99de7a1b2a6fe1cb5947548a
FantasticBoyZ <khoidragonslayer@gmail.com>
2021-10-31 16:48:45 +0700

    init cart layout and checkout layout

---
+++ app/src/main/java/fptu/ninhtbm/thebookshop/ui/home/CheckoutActivity.java
@@ -0,0 +1,19 @@
+package fptu.ninhtbm.thebookshop.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import fptu.ninhtbm.thebookshop.R;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+    }
+}
