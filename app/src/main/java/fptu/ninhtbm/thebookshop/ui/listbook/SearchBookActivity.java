ba5b8b30a12f842804d3e6c4d565d2a8d9a3d8ca
ninh137 <adzekk01@gmail.com>
2021-11-03 23:25:40 +0700

    Add Search book and list book

---
+++ app/src/main/java/fptu/ninhtbm/thebookshop/ui/listbook/SearchBookActivity.java
@@ -0,0 +1,16 @@
+package fptu.ninhtbm.thebookshop.ui.listbook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fptu.ninhtbm.thebookshop.R;
+
+public class SearchBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_book);
+    }
+}
